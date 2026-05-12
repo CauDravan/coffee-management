f0e78c78137666cf73f3d1c61ee18721783cd228
CauDravan <kindgate000@gmail.com>
2026-05-12 21:23:17 +0700

    connect db

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Coffee Shop API is running...");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is working!"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
