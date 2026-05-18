af87343afe57f19472adb78de2d84138cecdf897
CauDravan <kindgate000@gmail.com>
2026-05-18 18:53:57 +0700

    feat: add product, category, cart, order, and admin routes to the API

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,14 @@ import cors from "cors";
 
 import connectDB from "./src/config/db.js";
 
+/// Routes
+import authRoutes from "./src/routes/authRoutes.js";
+import productRoutes from "./src/routes/productRoutes.js";
+import categoryRoutes from "./src/routes/categoryRoutes.js";
+import cartRoutes from "./src/routes/cartRoutes.js";
+import orderRoutes from "./src/routes/orderRoutes.js";
+import adminRoutes from "./src/routes/adminRoutes.js";
+
 dotenv.config();
 
 connectDB();
@@ -13,6 +21,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/// Test Route
 app.get("/", (req, res) => {
   res.send("Coffee Shop API is running...");
 });
@@ -23,6 +32,14 @@ app.get("/api/test", (req, res) => {
     message: "Backend is working!"
   });
 });
+
+/// API Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/admin", adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 
